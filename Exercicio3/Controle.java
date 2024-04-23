adee12be4eb9e6cce8570be4b0e4cec0dd5a3607
CaioBalc <balczarekcaio@gmail.com>
2024-04-23 16:42:41 -0300

    Exercício 3

---
+++ Exercicio3/Controle.java
@@ -0,0 +1,21 @@
+package Exercicio3;
+
+public class Controle {
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa();
+        p1.setNome("Nome1");
+        p1.setEndereco("Endereço1");
+        p1.setTelefone("Telefone1");
+        System.out.println(p1.getNome());
+        System.out.println(p1.getEndereco());
+        System.out.println(p1.getTelefone());
+
+        Pessoa p2 = new Pessoa();
+        p2.setNome("Nome2");
+        p2.setEndereco("Endereço2");
+        p2.setTelefone("Telefone2");
+        System.out.println(p2.getNome());
+        System.out.println(p2.getEndereco());
+        System.out.println(p2.getTelefone());
+    }
+}
---
+++ Exercicio3/Fornecedor.java
@@ -0,0 +1,10 @@
+package Exercicio3;
+
+public class Fornecedor {
+    private double valorCredito;
+    private double valorDivida;
+
+    public void obterSaldo() {
+        
+    }
+}
