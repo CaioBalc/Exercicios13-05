d60a44edb6f2321dff89eccf9096a20fc4374211
CaioBalc <balczarekcaio@gmail.com>
2024-05-02 12:42:11 -0300

    Exercício 3

--- Exercicio3/Controle.java
+++ Exercicio3/Controle.java
@@ -3,6 +3,7 @@ package Exercicio3;
 public class Controle {
     public static void main(String[] args) {
         Pessoa p1 = new Pessoa();
+        System.out.println("Pessoas:");
         p1.setNome("Nome1");
         p1.setEndereco("Endereço1");
         p1.setTelefone("Telefone1");
@@ -11,11 +12,23 @@ public class Controle {
         System.out.println(p1.getTelefone());
 
         Pessoa p2 = new Pessoa();
-        p2.setNome("Nome2");
+        p2.setNome("\nNome2");
         p2.setEndereco("Endereço2");
         p2.setTelefone("Telefone2");
         System.out.println(p2.getNome());
         System.out.println(p2.getEndereco());
         System.out.println(p2.getTelefone());
+
+        Fornecedor f = new Fornecedor();
+        System.out.println("\nFornecedores:");
+        f.setNome("Fornecedor 1");
+        f.setEndereco("Endereço do Fornecedor 1");
+        f.setTelefone("Telefone do Fornecedor 1");
+        f.setValorCredito(1000.0);
+        f.setValorDivida(300.0);
+        System.out.println("Nome: " + f.getNome());
+        System.out.println("Endereço: " + f.getEndereco());
+        System.out.println("Telefone: " + f.getTelefone());
+        System.out.println("Saldo: " + f.obterSaldo());
     }
 }
--- Exercicio3/Fornecedor.java
+++ Exercicio3/Fornecedor.java
@@ -1,10 +1,26 @@
 package Exercicio3;
 
-public class Fornecedor {
+public class Fornecedor extends Pessoa {
     private double valorCredito;
     private double valorDivida;
 
-    public void obterSaldo() {
-        
+    public double getValorCredito() {
+        return valorCredito;
+    }
+
+    public void setValorCredito(double valorCredito) {
+        this.valorCredito = valorCredito;
+    }
+
+    public double getValorDivida() {
+        return valorDivida;
+    }
+
+    public void setValorDivida(double valorDivida) {
+        this.valorDivida = valorDivida;
+    }
+
+    public double obterSaldo() {
+        return valorCredito - valorDivida;
     }
 }
