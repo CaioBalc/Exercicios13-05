978a47c61df3cb03adc86540579d14a509b24589
CaioBalc <balczarekcaio@gmail.com>
2024-05-04 12:48:13 -0300

    Exercício 10

--- Exercicio10/Papagaio.java
+++
@@ -1,7 +0,0 @@
-package Exercicio10;
-
-public class Papagaio extends Animal{
-    public Papagaio(String nome, String som) {
-        super("Papagaio", "Java é legal");
-    }
-}
--- Exercicio10/Zoologico.java
+++ Exercicio10/Zoologico.java
@@ -1,10 +1,57 @@
 package Exercicio10;
 
 public class Zoologico {
+    public static void main(String[] args) {
+        Zoologico zoologico = new Zoologico();
+        zoologico.verAnimal();
+    }
+
+    private Animal[] jaulas;
+
     public Zoologico() {
-        Animal[] jaulas = new Animal[10];
-        
+        jaulas = new Animal[10];
         String[] animais = {"Capivara", "Chimpanzé", "Crocodilo", "Elefante", "Girafa", "Gorila", "Hipopótamo", "Leão", "Papagaio", "Veado"};
-        
+
+        for (int i = 0; i < animais.length; i++) {
+            jaulas[i] = new Animal(animais[i], getSom(animais[i]));
+        }
+    }
+
+    private String getSom(String animal) {
+        switch (animal) {
+            case "Capivara":
+                return "Ahn";
+            case "Chimpanzé":
+                return "Urrú";
+            case "Crocodilo":
+                return "Roar";
+            case "Elefante":
+                return "Fuumm";
+            case "Girafa":
+                return "Iiirrrrí";
+            case "Gorila":
+                return "Rúrú";
+            case "Hipopótamo":
+                return "Roonn";
+            case "Leão":
+                return "Groar";
+            case "Papagaio":
+                return "Java é legal";
+            case "Veado":
+                return "Inhóóó";
+            default:
+                return "";
+        }
+    }
+
+    public void verAnimal() {
+        for (Animal animal : jaulas) {
+            System.out.print("O som do animal " + animal.getNome() + " é: ");
+            animal.emitirSom();
+            
+            if (!animal.getNome().equals("Papagaio")) {
+                System.out.println("O animal está correndo!");
+            }
+        }
     }
 }
